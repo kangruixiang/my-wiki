d8c4d9bf05a266fac549f8c07df16fd8b09ec461
kangruixiang <thisispiggy@gmail.com>
2020-10-31 22:32:16 -0400

    updated paths

--- build2.js
+++ build2.js
@@ -4,6 +4,7 @@ const fs = require("fs");
 const glob = require("glob");
 
 const files = glob.sync(path.join(__dirname, "docs", "*.md"));
+const paths = [];
 
 files.forEach((file) => {
   // let content = fs.readFileSync(file, "utf8");
@@ -11,4 +12,11 @@ files.forEach((file) => {
   let filename = path.basename(file);
   console.log(dir, filename);
   fs.renameSync(file, dir + "/" + filename.toLowerCase().replace(/\s+/g, "-"));
+
+  paths.push("/docs/" + filename.replace(".md", ""));
 });
+
+// write paths.json for search index
+
+const content = `const paths = ${JSON.stringify(paths)}`;
+fs.writeFileSync("paths.js", content, "utf8");
